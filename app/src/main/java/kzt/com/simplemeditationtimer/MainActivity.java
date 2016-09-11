d754ccc1b6787640a83a3c9ed034788591da94c3
Kazuto Seito <torinokonoko@gmail.com>
2016-09-11 17:11:59 +0900

    show three buttons

--- app/src/main/java/kzt/com/simplemeditationtimer/MainActivity.java
+++ app/src/main/java/kzt/com/simplemeditationtimer/MainActivity.java
@@ -1,19 +1,27 @@
 package kzt.com.simplemeditationtimer;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jesusm.holocircleseekbar.lib.HoloCircleSeekBar;
 
-public class MainActivity extends AppCompatActivity {
+import kzt.com.simplemeditationtimer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements MainActivityHandlers {
 
     private TextView timerText;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setHandler(this);
 
         timerText = (TextView) findViewById(R.id.timer_text);
 
@@ -34,5 +42,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void changeButton(boolean flag) {
+        if (flag) {
+
+        }
+    }
+
+    @Override
+    public void clickStartTimer(View view) {
+        Toast.makeText(this, "くりく", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void clickStopTimer(View view) {
+
+    }
+
+    @Override
+    public void clickPauseTimer(View view) {
+
     }
 }
---
+++ app/src/main/java/kzt/com/simplemeditationtimer/MainActivityHandlers.java
@@ -0,0 +1,12 @@
+package kzt.com.simplemeditationtimer;
+
+import android.view.View;
+
+/**
+ * Created by k-seito on 2016/09/11.
+ */
+public interface MainActivityHandlers {
+    void clickStartTimer(View view);
+    void clickStopTimer(View view);
+    void clickPauseTimer(View view);
+}
