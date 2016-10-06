de779357ec1e213364d460e0b0d1411a96133d70
Kazuto Seito <torinokonoko@gmail.com>
2016-10-06 12:16:03 +0900

    瞑想中はタイマー操作不可

--- app/src/main/java/kzt/com/simplemeditationtimer/MainActivity.java
+++ app/src/main/java/kzt/com/simplemeditationtimer/MainActivity.java
@@ -1,7 +1,10 @@
 package kzt.com.simplemeditationtimer;
 
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -13,21 +16,23 @@ import kzt.com.simplemeditationtimer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements MainActivityHandlers {
 
+    private static final String PREF_SET_MINUTE = "set_time";
+
     private TextView timerText;
     private ActivityMainBinding binding;
-    private boolean isMeditating = false;
+    private CountDownTimer timer;
+
+    private int tempTimeInMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setHandler(this);
 
         timerText = (TextView) findViewById(R.id.timer_text);
 
-        HoloCircleSeekBar seekbar = (HoloCircleSeekBar) findViewById(R.id.seekbar);
-        seekbar.setOnSeekBarChangeListener(new HoloCircleSeekBar.OnCircleSeekBarChangeListener() {
+        binding.seekbar.setOnSeekBarChangeListener(new HoloCircleSeekBar.OnCircleSeekBarChangeListener() {
             @Override
             public void onProgressChanged(HoloCircleSeekBar holoCircleSeekBar, int i, boolean b) {
                 timerText.setText(CommonUtil.convertTime(i));
@@ -45,20 +50,72 @@ public class MainActivity extends AppCompatActivity implements MainActivityHandl
         });
     }
 
+    private void startTimer(int timeMillis) {
+        binding.seekbar.setMax(3600000);
+        binding.seekbar.setValue(timeMillis);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.edit().putInt(PREF_SET_MINUTE, timeMillis / 60 / 1000).apply();
+
+        binding.overlay.setVisibility(View.VISIBLE);
+
+        timer = new CountDownTimer(timeMillis, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long second = millisUntilFinished / 1000;
+                binding.timerText.setText(String.format("%1$02d:%2$02d",
+                        second / 60, second % 60));
+                binding.seekbar.setValue(millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(MainActivity.this, "終了", Toast.LENGTH_SHORT).show();
+            }
+        };
+        timer.start();
+    }
+
+    private void stopTimer() {
+        binding.seekbar.setMax(60);
+        binding.overlay.setVisibility(View.INVISIBLE);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int minute = prefs.getInt(PREF_SET_MINUTE, 0);
+        binding.timerText.setText(CommonUtil.convertTime(minute));
+        timer.cancel();
+    }
+
+    private void pauseTimer() {
+        if (tempTimeInMillis == 0) {
+            //pause
+            binding.pauseButton.setImageResource(R.mipmap.ic_start);
+            tempTimeInMillis =  binding.seekbar.getValue();
+            timer.cancel();
+
+        } else {
+            //reset pause
+            binding.pauseButton.setImageResource(R.mipmap.ic_pause);
+            startTimer(tempTimeInMillis);
+            tempTimeInMillis = 0;
+        }
+    }
+
     @Override
     public void clickStartTimer(View view) {
         AnimationUtils.startMeditation(binding.startButton, binding.stopButton, binding.pauseButton);
-        isMeditating = true;
+
+        startTimer(binding.seekbar.getValue() * 60 * 1000);
     }
 
     @Override
     public void clickStopTimer(View view) {
         AnimationUtils.stopMeditation(binding.startButton, binding.stopButton, binding.pauseButton);
-        isMeditating = false;
+
+        stopTimer();
     }
 
     @Override
     public void clickPauseTimer(View view) {
-        Toast.makeText(this, "ぽーず", Toast.LENGTH_SHORT).show();
+        pauseTimer();
     }
 }
